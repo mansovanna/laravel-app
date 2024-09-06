f05069fea516caa8f9b95f2e412916478b4b4538
kmn-kh <kmnpro459@gmail.com>
2024-09-06 11:51:57 +0700

    fix route login register and page menu

--- resources/views/AdminLTE/pages/examples/login.blade.php
+++ resources/views/AdminLTE/pages/examples/login.blade.php
@@ -18,101 +18,50 @@
 </head>
 
 <body class="hold-transition login-page">
-<<<<<<< HEAD
-<div class="login-box">
-  <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
-
-      <form method="post" action="{{route('adminLogin')}}" enctype="multipart/form-data">
-        @csrf
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-
-      {{-- <div class="social-auth-links text-center mb-3">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div>
-      <!-- /.social-auth-links -->
-
-      <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p> --}}
-      <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
-      </p>
-=======
   <div class="login-box">
     <div class="login-logo">
       <a href="../../index2.html"><b>Admin</b>LTE</a>
->>>>>>> d12fe341ca8623acf5810e2010aacb5b03bfb4d5
     </div>
     <!-- /.login-logo -->
     <div class="card">
       <div class="card-body login-card-body">
         <p class="login-box-msg">Sign in to start your session</p>
         @if($errors->has('message'))
-            <div class="alert alert-danger">
-                <span>{{ $errors->first('message') }}</span>
-            </div>
-        @endif
+      <div class="alert alert-danger">
+        <span>{{ $errors->first('message') }}</span>
+      </div>
+    @endif
         <form action="/admins/login" method="post" enctype="multipart/form-data">
           @csrf
-          <div class="input-group mb-3">
-            <input type="email" class="form-control" placeholder="Email" name="email">
+          <div class="input-group">
+            @php 
+                $rememberEmail;
+        if ($errors->first('dataEmail')) {
+          $rememberEmail = $errors->first('dataEmail');
+        } else {
+          $rememberEmail = old('email');
+        }
+        @endphp
+            <input type="email" class="form-control" placeholder="Email" name="email" value="{{ $rememberEmail }}">
             <div class="input-group-append">
               <div class="input-group-text">
-                <span class="fas fa-envelope">{{$errors->first('email')}}</span>
+                <span class="fas fa-envelope"></span>
               </div>
             </div>
           </div>
-          <div class="input-group mb-3">
-            <input type="password" class="form-control" placeholder="Password" name="password">
+          <span class="text-danger">{{$errors->first('email')}}</span>
+          <div class="mb-3"></div>
+          <div class="input-group">
+            <input type="password" class="form-control" placeholder="Password" name="password"
+              value="{{ old('password') }}">
             <div class="input-group-append">
               <div class="input-group-text">
-                <span class="fas fa-lock">{{$errors->first('password')}}</span>
+                <span class="fas fa-lock"></span>
               </div>
             </div>
           </div>
+          <span class="text-danger">{{$errors->first('password')}}</span>
+          <div class="mb-3"></div>
           <div class="row">
 
             <!-- /.col -->
